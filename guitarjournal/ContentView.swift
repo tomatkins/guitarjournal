a2efe5399d5f8b685783e55c9d4fcfd8fdda743b
tomatkins <treat@mac.com>
2020-01-07 20:57:29 -0500

    Initial Commit

---
+++ guitarjournal/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  guitarjournal
+//
+//  Created by Tom Atkins on 1/7/20.
+//  Copyright © 2020 High Strung Productions LLC. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
